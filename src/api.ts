1900baaeb216394f5b0ec544fe9735209e5513c4
mr. keith <keith@hoerling.com>
2019-10-25 20:22:36 -0700

    wip: portable, secure implementation of request

--- src/api.ts
+++ src/api.ts
@@ -1,48 +1,17 @@
-import * as queryString from 'query-string'
-import * as crypto from 'crypto'
 import { Push, PushResponse } from './index'
 import { request, shopifyDomainFrom } from './helpers'
 
-// pull implementation-dependent fns in
-
 // implement push interface
 export const push = (store: string, secret: string): Push => {
-  const shop = shopifyDomainFrom(store),
-    seed = generateSeed(),
-    url = `?seed=${seed}`,
-    sig = signature(secret, seed, url)
+  const shop = shopifyDomainFrom(store)
   return {
     token: async (token: string, customer?: string) => {
       // securely upsert token & customer
-      return request(shop, '/push-token', sig, seed, { token, customer })
+      return request(shop, secret, '/push-token', { token, customer })
     },
     message: (title: string, body: string, data: object) => {
       // push message
-      return request(shop, '/push-message', sig, seed, { title, body, data })
+      return request(shop, secret, '/push-message', { title, body, data })
     }
   }
-}
-
-// helper fns
-// --------
-function generateSeed(): string {
-  return ((crypto.randomFillSync(new Uint32Array(1))[0] / 4294967295) * 100)
-    .toString()
-    .replace(/[^\d]/, '')
-}
-
-// generate signature required for request
-function signature(secret: string, seed: string, url: string) {
-  const query = queryString.parse(url),
-    q = Object.assign({}, query),
-    sortedParams = Object.keys(q)
-      .sort()
-      .reduce((m, a) => {
-        m.push(`${a}=${q[a]}`)
-        return m
-      }, [])
-      .join(''),
-    hmac = crypto.createHmac('sha256', secret)
-  hmac.update(sortedParams)
-  return hmac.digest('hex')
 }
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,8 @@
 import * as queryString from 'query-string'
 import fetch from 'node-fetch'
 
+const forge = require('node-forge')
+
 function shopifyDomainFrom(domain: string) {
   if (domain.indexOf('myshopify.com') >= 0) {
     return domain
@@ -9,15 +11,41 @@ function shopifyDomainFrom(domain: string) {
   }
 }
 
+function generateSeed() {
+  const md = forge.md.sha256.create(),
+    rnd = forge.random.getBytesSync(8)
+  md.update(rnd)
+  return md.digest().toHex()
+}
+
+// generate signature required for request
+function signature(secret: string, seed: string, url: string) {
+  const query = queryString.parse(url),
+    q = Object.assign({}, query),
+    sortedParams = Object.keys(q)
+      .sort()
+      .reduce((m, a) => {
+        m.push(`${a}=${q[a]}`)
+        return m
+      }, [])
+      .join(''),
+    hmac = forge.hmac.create()
+  hmac.start('sha256', secret)
+  hmac.update(sortedParams)
+  return hmac.digest().toHex()
+}
+
 async function request(
   shop: string,
+  secret: string,
   endpoint: string,
-  sig: string,
-  seed: string,
   body: object
 ) {
   try {
-    const res = await fetch(
+    const seed = generateSeed(),
+      url = `?seed=${seed}`,
+      sig = signature(secret, seed, url),
+      res = await fetch(
         `https://${shop}/apps/dimensionauth/api${endpoint}?${queryString.stringify(
           {
             seed,
